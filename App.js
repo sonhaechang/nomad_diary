1b22dae145017684166e088514caa94de73aa98a
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-05-25 15:50:49 +0900

    feat: Realm SDK
    1. install Realm and AppLoading
    2. Realm 초기 설정 및 AppLoading을 사용해서 로딩되면서 DB 연동되게? 작성

--- App.js
+++ App.js
@@ -1,10 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native'
 import Navigator from './navigator';
 
+import AppLoading from 'expo-app-loading';
+
+import Realm from 'realm';
+
+
+const FeelingSchema = {
+	name: 'Feeling',
+	properties: {
+		_id: 'int',
+		emotion: 'string',
+		message: 'string',
+	},
+	primaryKey: '_id',
+};
 
 export default function App() {
+	const [ready, setReady] = useState(false);
+
+	const startLoading = async () => {
+		const realm = await Realm.open({
+		  	path: 'nomadDiaryDB',
+		  	schema: [FeelingSchema],
+		});
+	};
+
+	const onFinish = () => setReady(true);
+
+	if (!ready) { 
+		return <AppLoading 
+			startAsync={startLoading} 
+			onFinish={onFinish}
+			onError={console.error}
+		/>;
+	};
+
 	return (
 		<NavigationContainer>
 			<Navigator /> 
